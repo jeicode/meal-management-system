d55dda655b177b103271a0afd4acf588513f4d5e
Jeison Obando <jeison.obando@paxzu.co>
2025-10-08 12:13:34 -0500

    upgrade time and test

--- kitchen-ms/src/core/jobs/order.jobs.ts
+++ kitchen-ms/src/core/jobs/order.jobs.ts
@@ -1,11 +1,12 @@
-// import { updateOrderPendingToDelivered } from "../../modules/orders/domain/repositories/orders.repository";
-// import { logError } from "../../shared/utils/logs.utils";
+import { updateOrderPendingToDelivered } from '../../modules/orders/domain/repositories/orders.repository';
+import { logError } from '../../shared/utils/logs.utils';
 
-// const timeToPrepare = 8000;
+const timeToPrepare = 10000;
 export const prepareOrdersJob = async () => {
-  // try {
-  //     await updateOrderPendingToDelivered()
-  // }
-  // catch (error: any) {logError(error.message)}
-  // setTimeout( async () => await prepareOrdersJob(), timeToPrepare);
+  try {
+    await updateOrderPendingToDelivered();
+  } catch (error: any) {
+    logError(error.message);
+  }
+  setTimeout(async () => await prepareOrdersJob(), timeToPrepare);
 };
