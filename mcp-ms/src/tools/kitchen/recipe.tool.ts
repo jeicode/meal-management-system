70f9f15df9bc1c99e634f719d3fd640c6e652d42
Jeison Obando <jeison.obando@paxzu.co>
2025-10-08 19:14:15 -0500

    improve recipe tool

--- mcp-ms/src/tools/kitchen/recipe.tool.ts
+++ mcp-ms/src/tools/kitchen/recipe.tool.ts
@@ -5,24 +5,54 @@ export function createRecipeTool() {
   mcpServer.registerTool(
     'Crear receta',
     {
-      title: 'Crear receta',
+      title: 'Crear receta y sus ingredientes',
       description: 'Crear nueva receta en la base de datos',
-      inputSchema: { name: z.string() },
+      inputSchema: {
+        name: z
+          .string()
+          .describe(
+            'Nombre de la receta. Debe ir de acuerdo a los ingredientes que tiene. El nombre debe ser descriptivo. Ejemplo: "Ensalada de pollo"',
+          ),
+        ingredients: z.array(
+          z.object({
+            ingredientId: z.number().describe('ID del ingrediente'),
+            quantity: z.number().max(3).describe('Cantidad del ingrediente (1-3)'),
+          }),
+        ),
+      },
     },
 
-    async ({ name }) => {
+    async ({ name, ingredients }) => {
       // Validación adicional
       if (!name.trim()) {
         throw new Error("El parámetro 'name' es requerido");
       }
+      if (!ingredients || ingredients.length === 0) {
+        throw new Error("El parámetro 'ingredients' es requerido");
+      }
+
       try {
         const { data, error } = await supabase
           .from('Recipe')
           .insert([{ name: name.trim() }])
-          .select(); // Agregado para obtener los datos insertados
-
-        if (error) {
+          .select();
+        if (error || !data) {
           throw new Error(`Error al crear la receta: ${error.message}`);
+        }
+
+        const inserts = ingredients.map(ingredient => ({
+          recipeId: data[0].id,
+          ingredientId: ingredient.ingredientId,
+          quantity: ingredient.quantity,
+        }));
+
+        const { error: insertError } = await supabase
+          .from('RecipeIngredient')
+          .insert(inserts)
+          .select();
+
+        if (insertError) {
+          throw new Error(`Error al crear los ingredientes de la receta: ${insertError.message}`);
         }
 
         return {
